ba9947c94c826dcb33c274a8c298365cd80ff2cf
Phani Raj <phanirajuyn@gmail.com>
2015-09-04 11:33:48 -0400

    Added tests

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import org.junit.Test;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(false, true);
+	}
+
+}
